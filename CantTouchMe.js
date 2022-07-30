09bae6e9d18b447e111482bb26e833d04aea1c63
Martijn Bleyen <martijn.bleyen@gmail.com>
2022-07-30 22:38:21 +0200

    feat: added animation

--- CantTouchMe.js
+++ CantTouchMe.js
@@ -1,3 +1,5 @@
+import { toDegrees, toRadians } from "./utils.js";
+
 export default class CantTouchMe {
     constructor(el, options = {}) {
         this.el = el;
@@ -5,17 +7,26 @@ export default class CantTouchMe {
         this.basePos = this.el.getBoundingClientRect();
         this.elW = this.basePos.width;
         this.elH = this.basePos.height;
+        this.baseBgColor = this.el.style.backgroundColor;
 
-        this.radiusX = (this.elW / 2) * 1.25;
-        this.radiusY = (this.elH / 2) * 1.25;
-
+        this.setOptions(options);
 
         CantTouchMe.init();
         CantTouchMe.add(this);
     }
 
+    setOptions(options) {
+        this.options = {
+            radiusPerc: 125,
+            ...options,
+        };
+
+        this.radiusX = ((this.elW / 2) * this.options.radiusPerc) / 100;
+        this.radiusY = ((this.elH / 2) * this.options.radiusPerc) / 100;
+    }
+
     check(mousePos) {
-        console.log("CHECKING", this);
+        // console.log("CHECKING", this);
         const basePos = this.basePos;
         const yPos = basePos.y + basePos.height / 2 - mousePos.y;
         const xPos = basePos.x + basePos.width / 2 - mousePos.x;
@@ -24,17 +35,25 @@ export default class CantTouchMe {
         const radius = Math.abs(yPos) <= this.elH ? this.radiusX : this.radiusY;
 
         const angle = toDegrees(Math.atan2(yPos, xPos));
-        console.log({ xPos, yPos, diagonal, angle });
+        // console.log({ xPos, yPos, diagonal, angle });
         const ballX = (radius - diagonal) * Math.cos(toRadians(angle));
         const ballY = (radius - diagonal) * Math.sin(toRadians(angle));
-        console.log({ xPos, yPos, diagonal, angle });
         requestAnimationFrame(() => {
             if (diagonal > radius) {
-                ball.style.transform = `translate(0px, 0px)`;
-            } else {
-                ball.style.transform = `translate(${ballX}px, ${ballY}px)`;
-            }
+                // this.el.style.transform = `translate(0px, 0px)`;
+                // this.el.animate({ transform: "translate(0px, 0px)" }, { duration: 300, iterations: 1 });
+                const anim = this.el.animate({ transform: "translate(0px, 0px)" }, { duration: 100, iterations: 1 });
+                anim.commitStyles();
 
+                this.el.style.backgroundColor = this.baseBgColor;
+                this.el.style.zIndex = "unset";
+            } else {
+                // this.el.style.transform = `translate(${ballX}px, ${ballY}px)`;
+                const anim = this.el.animate({ transform: `translate(${ballX}px, ${ballY}px)` }, { duration: 100, iterations: 1 });
+                anim.commitStyles();
+                this.el.style.backgroundColor = this.options.bgColor;
+                this.el.style.zIndex = 4;
+            }
         });
     }
 
@@ -52,8 +71,10 @@ export default class CantTouchMe {
 
         window.addEventListener("mousemove", (e) => {
             const mousePos = { x: e.clientX, y: e.clientY };
+            // console.log("MOUSE", mousePos);
+            // console.log(this.elements);
             this.elements.forEach((el) => {
-                console.log({el});
+                // console.log({el});
                 el.check(mousePos);
             });
         });
--- index.js
+++ index.js
@@ -2,3 +2,47 @@ import CantTouchMe from "./CantTouchMe.js";
 
 const el = document.getElementById("ball");
 const ctm = new CantTouchMe(el);
+
+// makeElement(100, 100, 100);
+
+// const size = 100;
+// const maxX = Math.ceil(window.innerWidth / size);
+// const maxY = Math.ceil(window.innerHeight / size);
+
+// const allElements = [];
+// for (let i = 0; i < maxY; i++) {
+//     const y = i * size;
+//     for (let j = 0; j < maxX; j++) {
+//         const x = j * size;
+//         makeElement(size, x, y);
+//     }
+// }
+
+// function makeElement(size, x, y) {
+//     const el = document.createElement("div");
+//     el.style.width = size + "px";
+//     el.style.height = size + "px";
+//     el.style.border = "1px solid rgba(255,255,255,0.2)";
+//     // el.style.borderRadius = "50%";
+//     el.style.backgroundColor = "#000";
+//     el.style.position = "absolute";
+//     el.style.left = x + "px";
+//     el.style.top = y + "px";
+//     document.body.appendChild(el);
+//     const radius = Math.random() * 150 + 400;
+//     const ctm = new CantTouchMe(el, { radiusPerc: radius , bgColor : "#9999ff" });
+//     allElements.push(ctm);
+//     return el;
+// }
+
+// function resizeRadius() {
+//     allElements.forEach((el) => {
+//         const radius = Math.random() * 500 + 200;
+//         el.setOptions({ radiusPerc: radius });
+//     });
+//     setTimeout(() => {
+//         resizeRadius();
+//     }, 2000);
+// }
+
+// resizeRadius();
